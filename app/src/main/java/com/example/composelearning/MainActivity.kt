6cc8260cea65fb85bb48e43867e677eeacb2942e
sorok <soroka144@gmail.com>
2024-11-08 10:55:30 +0300

    2.3 Row, Box, Modifier

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -11,28 +11,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composelearning.ui.theme.ComposeLearningTheme
+import com.example.composelearning.ui.theme.CardBoxes
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TableRow()
+            //TableRow()
+            CardBoxes()
         }
     }
 }
 
-@Preview
 @Composable
 fun TableRow() {
     Column(
@@ -56,7 +54,7 @@ fun TableRow() {
                             .background(color = color),
                         contentAlignment = Alignment.Center
                     ) {
-                    Text("${i * k}")
+                        Text("${i * k}")
                     }
                 }
             }
@@ -64,6 +62,10 @@ fun TableRow() {
 
     }
 }
+
+
+
+
 
 
 
