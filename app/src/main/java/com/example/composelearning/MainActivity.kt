e2371ae480fdb2db84f63a5062228e057f345332
sorok <soroka144@gmail.com>
2024-11-08 09:43:43 +0300

    2.3 Row, Box, Modifier

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +27,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UserInfo("John", 29)
+            TableRow()
         }
     }
 }
 
 @Preview
 @Composable
-fun Greetings(){
-    UserInfo(name = "John", age = 29)
-}
-
-@Composable
-fun UserInfo(name: String,age:Int) {
-    Column {
-         List(10){
-             Text(
-                 text = "Hello $name! Тебе $age лет!",
-                 color = Color.Cyan
-             )
-         }
+fun TableRow() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    )
+    {
+        for (i in 1..10) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+            {
+                for (k in 1..10) {
+                    val color = if ((i + k) % 2 == 0) Color.Gray else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.LightGray)
+                            .background(color = color),
+                        contentAlignment = Alignment.Center
+                    ) {
+                    Text("${i * k}")
+                    }
+                }
+            }
+        }
 
     }
 }
+
+
 
